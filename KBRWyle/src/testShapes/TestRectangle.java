0aa5de1a4c5a6e8ec3e5a5acd1420bd1ee362adb
rjl5201 <rjl5201@gmail.com>
2016-08-03 21:23:06 -0400

    Missing file

---
+++ KBRWyle/src/testShapes/TestRectangle.java
@@ -0,0 +1,41 @@
+package testShapes;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+//import shapes.Rectangle;
+
+/*public class TestRectangle {
+
+	Rectangle tester = new Rectangle(2, 4);
+
+	@Test
+	public void testGetArea() {
+		double area = tester.getArea();
+		assertEquals(area, 8, .01);
+	}
+
+	@Test
+	public void testGetPerimeter() {
+		double perim = tester.getPerimeter();
+		assertEquals(perim, 12, .01);
+	}*/
+
+
+import shapes.Triangle;
+
+public class TestRectangle {
+
+	Triangle tester = new Triangle(3, 4, 7);
+
+	@Test
+	public void testGetArea() {
+		double area = tester.getArea();
+		assertEquals(area, 6, .01);
+	}
+
+	@Test
+	public void testGetPerimeter() {
+		double perim = tester.getPerimeter();
+		assertEquals(perim, 14, .01);
+	}
+}
