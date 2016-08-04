c9470dca92bcacb71de86081e540e82a6db443f2
rjl5201 <rjl5201@gmail.com>
2016-08-04 09:11:44 -0400

    Update

--- KBRWyle/src/testShapes/TestRectangle.java
+++ KBRWyle/src/testShapes/TestRectangle.java
@@ -3,9 +3,9 @@ package testShapes;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-//import shapes.Rectangle;
+import shapes.Rectangle;
 
-/*public class TestRectangle {
+public class TestRectangle {
 
 	Rectangle tester = new Rectangle(2, 4);
 
@@ -19,23 +19,5 @@ import org.junit.Test;
 	public void testGetPerimeter() {
 		double perim = tester.getPerimeter();
 		assertEquals(perim, 12, .01);
-	}*/
-
-import shapes.Triangle;
-
-public class TestRectangle {
-
-	Triangle tester = new Triangle(3, 4, 7);
-
-	@Test
-	public void testGetArea() {
-		double area = tester.getArea();
-		assertEquals(area, 6, .01);
-	}
-
-	@Test
-	public void testGetPerimeter() {
-		double perim = tester.getPerimeter();
-		assertEquals(perim, 14, .01);
 	}
 }
